90137467d09112864d5415b7b4a9c9436eb22f78
cordis-dev <darius@adroiti.com>
2022-03-01 11:52:46 +0200

    Delete insecure.tf

--- src/insecure.tf
+++
@@ -1,9 +0,0 @@
- variable "password" {
-   description = "The root password for our VM"
-   type        = string
-   default     = "p4ssw0rd"
- }
-
- resource "evil_corp" "virtual_machine" {
-    root_password = var.password
- }

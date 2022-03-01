c65e6ee6113b3d622a30cd3ba7ed29a820c7e260
cordis-dev <darius@adroiti.com>
2022-03-01 11:52:41 +0200

    Delete secure.tf

--- src/secure.tf
+++
@@ -1,8 +0,0 @@
- variable "password" {
-   description = "The root password for our VM"
-   type        = string
- }
-
- resource "evil_corp" "virtual_machine" {
-    root_password = var.password
- }

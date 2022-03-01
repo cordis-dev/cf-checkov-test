d576c090c709c5b85ec48a7e9b3987b8d04e0030
cordis-dev <darius@adroiti.com>
2022-03-01 15:05:38 +0200

    Update network-disable-rdp-from-internet-bad.tf

--- src/network-disable-rdp-from-internet-bad.tf
+++ src/network-disable-rdp-from-internet-bad.tf
@@ -8,16 +8,3 @@
       source_address_prefix       = "*"
       destination_address_prefix  = "*"
  }
-
- resource "azurerm_network_security_group" "example" {
-   name                = "tf-appsecuritygroup"
-   location            = azurerm_resource_group.example.location
-   resource_group_name = azurerm_resource_group.example.name
-
-   security_rule {
-     source_port_range           = "any"
-      destination_port_ranges     = ["3389"]
-      source_address_prefix       = "*"
-      destination_address_prefix  = "*"
-   }
- }

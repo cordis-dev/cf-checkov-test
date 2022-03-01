2831f575b0320923afa542c9b809455aceee04a1
cordis-dev <darius@adroiti.com>
2022-03-01 15:04:36 +0200

    Update enforce-https-good.tf

--- src/enforce-https-good.tf
+++ src/enforce-https-good.tf
@@ -2,4 +2,9 @@
     default_cache_behavior {
         viewer_protocol_policy = "redirect-to-https"
       }
+  
+   viewer_certificate {
+     cloudfront_default_certificate = true
+     minimum_protocol_version = "TLSv1.2_2021"
+   }  
  }

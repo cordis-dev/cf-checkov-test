a57fcb8f1889180f5c113074bcd3c3f507387cd0
cordis-dev <darius@adroiti.com>
2022-03-01 12:01:26 +0200

    Create network-ssh-blocked-from-internet-bad.tf

---
+++ src/network-ssh-blocked-from-internet-bad.tf
@@ -0,0 +1,10 @@
+ resource "azurerm_network_security_rule" "bad_example" {
+      name                        = "bad_example_security_rule"
+      direction                   = "Inbound"
+      access                      = "Allow"
+      protocol                    = "TCP"
+      source_port_range           = "*"
+      destination_port_range      = "22"
+      source_address_prefix       = "*"
+      destination_address_prefix  = "*"
+ }

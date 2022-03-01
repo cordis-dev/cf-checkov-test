32a8ac084ac994ecb028815270e6043ae46687c2
cordis-dev <darius@adroiti.com>
2022-03-01 14:59:23 +0200

    Update enforce-https-bad.tf

--- src/enforce-https-bad.tf
+++ src/enforce-https-bad.tf
@@ -1,6 +1,10 @@
  resource "aws_cloudfront_distribution" "bad_example" {
     default_cache_behavior {
         viewer_protocol_policy = "allow-all"
-        minimum_protocol_version = "TLSv1.2_2019"
       }
+  
+  viewer_certificate {
+    cloudfront_default_certificate = true
+	minimum_protocol_version = "TLSv1.2_2019"
+  }  
  }
